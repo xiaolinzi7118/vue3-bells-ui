6ee72e22c8b3726796f7eee3ebbf3009ad62ab1a
xiaolinzi7118 <1106147743@qq.com>
2021-06-17 16:01:49 +0800

    views的home和doc样式初始化

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './index.css'
-import HelloWorld from "./components/HelloWorld.vue";
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
     history,
     routes: [
-        { path: '/', component: HelloWorld }
+        { path: '/', component: Home },
+        { path: '/doc', component: Doc }
     ]
 })
 const app = createApp(App)
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>林子UI</h1>
+      <h2>一个厉害的UI框架</h2>
+      <p class="actions">
+        <a href="">GitHub</a>
+        <a href="">开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped lang='scss'>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
