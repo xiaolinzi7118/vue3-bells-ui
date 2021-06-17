92ce5d8da13671a90e3a2885d35fea34410c0976
xiaolinzi7118 <1106147743@qq.com>
2021-06-17 23:09:00 +0800

    button组件完成

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
-  <button class="linzi-button" :class="classes">
+  <button class="linzi-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="linzi-loadingIndicator"></span>
     <slot />
   </button>
 </template>
@@ -15,13 +16,26 @@ export default {
       type: String,
       default: "normal",
     },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
   },
-  setup(props, context) {
-    const { theme, size } = props;
+  setup(props) {
+    const { theme, size, level } = props;
     const classes = computed(() => {
       return {
         [`linzi-theme-${theme}`]: theme,
-        [`linzi-theme-${size}`]: size,
+        [`linzi-size-${size}`]: size,
+        [`linzi-level-${level}`]: level,
       };
     });
     return { classes };
@@ -34,6 +48,8 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .linzi-button {
   box-sizing: border-box;
   height: $h;
@@ -48,6 +64,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -89,6 +106,88 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.linzi-theme-button {
+    &.linzi-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.linzi-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.linzi-theme-link {
+    &.linzi-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.linzi-theme-text {
+    &.linzi-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.linzi-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.linzi-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.linzi-theme-link,
+  &.linzi-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .linzi-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: linzi-spin 1s infinite linear;
+  }
+}
+@keyframes linzi-spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
