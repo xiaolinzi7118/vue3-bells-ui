57f5cf4a70be28ff086813e88de51c382dbe9f52
xiaolinzi7118 <1106147743@qq.com>
2021-06-18 17:17:28 +0800

    Tabs样式

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Tabs 示例</div>
+  <div>
+    <h1>示例1</h1>
+    <Tabs>
+      <Tab title="导航1">内容1</Tab>
+      <Tab title="导航2">内容2</Tab>
+    </Tabs>
+  </div>
 </template>
+<script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+export default {
+  components: { Tabs, Tab },
+};
+</script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="linzi-tabs">
+    <div class="linzi-tabs-nav">
+      <div
+        class="linzi-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="linzi-tabs-content">
+      <component
+        class="linzi-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+      />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须是 Tab");
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return { defaults, titles };
+  },
+};
+</script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.linzi-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
