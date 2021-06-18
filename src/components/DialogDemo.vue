aaa473234b04833acf8d3ba3fd14330c614061fc
xiaolinzi7118 <1106147743@qq.com>
2021-06-18 09:49:55 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,10 @@
 <template>
   <div>Dialog 示例</div>
+  <Dialog></Dialog>
 </template>
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: { Dialog },
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+  <div>
+    <header>标题</header>
+    <main>第一行字</main>
+    <footer>
+      <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  components: { Button },
+};
+</script>
