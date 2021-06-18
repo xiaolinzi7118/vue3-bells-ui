108143d940d34b48f722fc9e813e5870f765d69b
xiaolinzi7118 <1106147743@qq.com>
2021-06-18 15:40:03 +0800

    完成一句话展示Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,6 @@
 <template>
   <div>Dialog 示例</div>
+  <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
   <Dialog v-model:visible="x" :ok="f1" :cancel="f2">
     <template v-slot:title>
@@ -10,11 +11,14 @@
       <div>hi2</div>
     </template>
   </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
 import { ref } from "vue";
+import { openDialog } from "../lib/openDialog";
 export default {
   components: { Dialog, Button },
   setup() {
@@ -26,7 +30,19 @@ export default {
       return false;
     };
     const f2 = () => {};
-    return { x, toggle, f1, f2 };
+    const showDialog = () => {
+      openDialog({
+        title: "标题",
+        content: "你好",
+        ok() {
+          console.log("ok");
+        },
+        cancel() {
+          console.log("cancel");
+        },
+      });
+    };
+    return { x, toggle, f1, f2, showDialog };
   },
 };
 </script>
