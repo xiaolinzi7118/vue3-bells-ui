27c8ad215a9307923c3e3464777f68ce26ad9f83
xiaolinzi7118 <1106147743@qq.com>
2021-06-19 15:50:13 +0800

    背景颜色

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <Topnav />
-    <div class="banner">
-      <h1>林子UI</h1>
-      <h2>一个厉害的UI框架</h2>
-      <p class="actions">
-        <a href="">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+    <div class="all-wrapper">
+      <Topnav />
+      <div class="banner">
+        <h1>林子UI</h1>
+        <h2>一个厉害的UI框架</h2>
+        <p class="actions">
+          <a href="">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -18,13 +20,20 @@ export default {
 };
 </script>
 <style scoped lang='scss'>
+.all-wrapper {
+  background: rgb(52, 244, 252);
+  background: linear-gradient(
+    90deg,
+    rgba(52, 244, 252, 1) 0%,
+    rgba(207, 99, 247, 1) 100%
+  );
+}
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
   > .actions {
     padding: 8px 0;
     a {
